3ab694d8443a02e2bca58fb6090e74d1dbd3ace4
Gerd Bleher <gerd@vavoor.de>
2016-04-17 21:29:33 +0200

    Wrong argument to ANTLRInputStream

--- Aufgabe_2/src/PipifaxCompiler.java
+++ Aufgabe_2/src/PipifaxCompiler.java
@@ -1,11 +1,12 @@
-
+import java.io.FileInputStream;
 import org.antlr.v4.runtime.ANTLRInputStream;
 import org.antlr.v4.runtime.CommonTokenStream;
 import org.antlr.v4.runtime.tree.ParseTree;
 
 class PipifaxCompiler {
   public static void main(String[] args) {
-    ANTLRInputStream input = new ANTLRInputStream(args[0]);
+  	FileInputStream is = new FileInputStream(args[0]);
+    ANTLRInputStream input = new ANTLRInputStream(is);
     PipifaxLexer lexer = new PipifaxLexer(input);
     CommonTokenStream tokens = new CommonTokenStream(lexer);
     PipifaxParser parser = new PipifaxParser(tokens);
